b31296340aa8d3b5aabadb70b4cc26af181798a0
shoaib94 <shoaib.khan@connvertex.com>
2017-10-10 16:56:48 +0530

    Base app ready

---
+++ client/component/header.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const Header = () => {
+    return (
+        <nav className="nav navbar-defauly">
+            <div className="navbar-default">
+                <a className="navbar-brand">
+                    LinkShortener
+                </a>
+            </div>
+        </nav>
+    )
+}
+
+export default Header;
--- client/main.js
+++ client/main.js
@@ -1,22 +1,19 @@
-import { Template } from 'meteor/templating';
-import { ReactiveVar } from 'meteor/reactive-var';
+import React from 'react';
+import ReactDOM from 'react-dom';
 
-import './main.html';
+import Header from './component/header';
+import LinkCreate from './component/link_create';
+import { Links } from "../imports/collections/links";
 
-Template.hello.onCreated(function helloOnCreated() {
-  // counter starts at 0
-  this.counter = new ReactiveVar(0);
-});
+const App = () => {
+  return (
+      <div>
+        <Header />
+        <LinkCreate />
+      </div>
+  );
+}
 
-Template.hello.helpers({
-  counter() {
-    return Template.instance().counter.get();
-  },
-});
-
-Template.hello.events({
-  'click button'(event, instance) {
-    // increment the counter when button is clicked
-    instance.counter.set(instance.counter.get() + 1);
-  },
+Meteor.startup(() => {
+  ReactDOM.render(<App />,document.querySelector('.container'));
 });
